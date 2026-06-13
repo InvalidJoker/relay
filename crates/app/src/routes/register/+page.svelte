880cb6983240be46be9323c95146522056595fe2
InvalidJoker <0limitdev@gmail.com>
2026-06-13 14:46:43 +0200

    fix size

--- crates/app/src/routes/register/+page.svelte
+++ crates/app/src/routes/register/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="bg-muted flex min-h-svh items-center justify-center p-6">
-    <div class="w-full max-w-md">
+    <div class="w-full max-w-4xl">
         <RegisterForm {form} />
     </div>
 </div>
